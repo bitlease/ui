ee86e6c24a39c5e8afb541c8975feb7bd3358b20
Elendu Ifeanyi <goodchild66@gmail.com>
2023-05-01 20:22:03 +0100

    integrated dashboard app and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import Home from './pages/Home';
-import Main from './pages/Main';
-import Bitlease from './pages/Bitlease';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+// import Main from "./pages/Main";
+import Bitlease from "./pages/Bitlease";
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
+<>
+    {/* <Navbar /> */}
     <Routes>
-
-      <Route path="Home" element={<Bitlease />} />
+      <Route path="Main" element={<Bitlease />} />
       <Route path="Home" element={<Home />} />
-      <Route path="/*" element={<Main />} />
+      <Route path="/*" element={<Home />} />
     </Routes>
+    {/* <Footer /> */}
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,45 @@
-import React from "react";
-import { BiMenuAltLeft } from 'react-icons/bi'
-import { Stack, Button } from '@mui/material';
-function Navbar() {
-    return (
-        <div className="w-full h-[5rem] bg-sky-500 py-3 px-4 flex justify-between items-center">
-           <div className="flex justify-between w-auto p-2 items-center">
-            <BiMenuAltLeft fontSize={45} fontWeight={700} className="bg-white rounded-md p-1 hover:bg-sky-200"/>
-            <p className="m-3 font-bold">BitLease</p>
-           </div>
-            <button className="bg-sky-700 p-2 rounded-md text-white px-6 hover:bg-sky-800">Connect Wallet</button>
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
+import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navRef = useRef(null);
+
+  const showNav = () => {
+    navRef.current?.classList.toggle("responsive-nav");
+  };
+
+  return (
+    <div className="bg-[#2C3333] nav-container">
+      <nav className="nav-holder flex items-center justify-between text-white py-8 max-w-[1024px] mx-[auto]">
+        <div className="h2-holder">
+          <h2 className="text-2xl">Bitlease</h2>
         </div>
-    );
-}
+
+        <ul className="nav-ul flex ml-[auto] items-center" ref={navRef}>
+          <li>
+            <a href="#">Team</a>
+          </li>
+          <li>
+            <Link to="/main" className="ml-6 border-[#0E8388] border-2 py-2 px-4 rounded-xl">
+              Open App
+            </Link>
+          </li>
+
+          {/* close button */}
+          <button onClick={showNav} className="nav-btn nav-btn-close">
+            <FaTimes />
+          </button>
+        </ul>
+
+        {/* put open button here */}
+        <button onClick={showNav} className="nav-btn">
+          <FaBars />
+        </button>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
